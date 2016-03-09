c8f5cb081907fe7cccfe191a89fc122070eff0b6
Andy93 <oneillandrew101@gmail.com>
2016-03-09 15:27:34 +0000

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,48 @@
+function init(){
+    var editor = ace.edit("aceEditor");
+    //editor.setValue("new code here");
+      editor.setTheme("ace/theme/twilight");
+      editor.session.setMode("ace/mode/javascript");
+      var session = editor.getSession();
+      session.setUseWrapMode(true);
+      session.setUseWorker(false);
+}
+
+function socket(){
+	try{
+      var socket = io.connect('http://127.0.0.1:8080');
+    } catch(e){
+
+    }
+}
+
+function setCode(){
+	try{
+      var socket = io.connect('http://127.0.0.1:8080');
+    } catch(e){
+
+    }
+	if(socket !== undefined){
+      //put latest string in DB to ACE
+      socket.on('output', function(data){
+        if(data.length){
+          var x = data.length-1;
+          editor.setValue(data[x].code);
+        }
+      });
+	}
+}
+
+function sendCode(){
+	try{
+      var socket = io.connect('http://127.0.0.1:8080');
+    } catch(e){
+
+    }
+	if(socket !== undefined){
+		var manip = editor.getSession().getValue();
+	    socket.emit('input',{
+	      code:manip
+	    })
+	}
+}
