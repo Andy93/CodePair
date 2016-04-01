b47b95379277ebb466964bdd5d546f5e8e4d3394
Andy93 <oneillandrew101@gmail.com>
2016-04-01 12:48:49 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -52,16 +52,29 @@ function sendCode(){
 // create random number from scratch 
 // take another word then concatenate and return
 function createString() {
- 	var hash;
-	var firstWord, secondWord, number;
+  var hash;
+  var firstWord = randomWord();
+  var secondWord = randomWord();
+  var number = Math.floor(Math.random()*999)+1;
 
-	var selectorOne =math.random();
-	var selectorTwo =math.random();
-	number = math.random();
+  hash = firstWord + number + secondWord;
+  return hash.toString();
+} 
 
-	hash= firstWord + number + secondWord;
-	return hash;
-}  
+function randomWord(){
+  var words = ["Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blueberry", "Cherry", "Grapefruit", "Lemon", "Lime",
+                "Coconut","Kiwi","Peach","Pear","Pineapple","Melon","Watermelon","Raspberry","Strawberry","Hanger",
+                "Grape","Plum","London","Dublin","Moscow","Berlin","Madrid","Paris","Stockholm","Vienna",
+                "Chair","Texas","California","Nevada","Florida","Montana","Bravo","Delta","Echo","Hotel",
+                "Tango","Whiskey","Foxtrot","Golf","Zulu","Yankee","Magnet","Button","Watch","Red",
+                "White","Green","Black","Yellow","Grey","Blue","Pink","Purple","Diary","Bottle",
+                "Water","Fire","Wind","Sweet","Sugar","Stamp","Brush","Small","Medium","Large",
+                "Brown","Piano","Guitar","Canvas","Carrot","Mouse","Dog","Cat","Squirrel","Truck",
+                "Rabbit","Toothbrush","Chalk","Puddle","Elephant","Giraffe","Frog","Falcon","Eagle","Parrot",
+                "Shark","Tiger","Butterfly","Turtle","Snake","Fish","Whale","Walrus","Kangaroo","Wolverine"];
+  return words[(Math.floor(Math.random()*100)+0)];
+
+} 
 
 
 // function to collect code from editor for repo download, User can click button to download code changes
